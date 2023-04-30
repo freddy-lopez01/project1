8d976cb182f5439c24fb8bcfc9a1ab1bd4485055
Freddy Lopez <freddlopez01@gmail.com>
2023-04-30 11:09:38 -0700

    pushing completed uspv1.c file

--- uspsv1.c
+++ uspsv1.c
@@ -10,62 +10,192 @@
 #define UNUSED __attribute__((unused))
 #define MAX 128
 
-//void execWork()
+
+
+int parse(int fd, char ***arr){
+	char buf[MAX];
+	char word[MAX];
+	int i = 0;
+	int j;  
+
+	while((j = p1getline(fd, buf, BUFSIZ)) != 0){  // while there is still a next line
+		int k = 0; 
+		int index = 0; 
+		while((index = p1getword(buf, index, word)) != -1){ 
+			arr[i][k] = p1strdup(word);
+			k++; 
+		}
+		arr[i][k] = NULL;
+		i++; 
+	}
+	return 1; 
+}
+
+void executePrograms(char ***arr, int numPrograms){
+	int pid[numPrograms];
+
+	for(int i = 0; i < numPrograms; i++){
+
+		pid[i] = fork();
+		switch(pid[i]){
+		case -1: 
+			p1putstr(1, "error when forking()\n");
+			exit(EXIT_FAILURE);
+		case 0:
+			execvp(arr[i][0], arr[i]);
+		default:
+			wait(NULL);
+
+		}
+	}
+
+}
+
+
 
 
 int main(UNUSED int argc, char *argv[]){
 	/* 
-
 	Need to check that there are at least two arguments in the ./uspsv1 workload file
-
 	next if file is specified then open up the file and read the first line
-
 	once the whole line is read, pass in the command and its aguments 
-
-
 	*/
+	char word[MAX];
 	char buf[MAX];
-	char programs[MAX];
-	char arguments[MAX];
 
+	int pid;
+	char ***programs;
 
 	char *fname; 
-	UNUSED int quantum; 
+	int quantum; 
 	int fd; 
+	int qflag = 0; 
 
+
+
+	int c; 
+	//Check for usage errors that could occur
+    opterr = 0; 
+	while((c = getopt(argc, argv, "q:")) != -1)
+		switch(c){
+			case 'q':
+				qflag = 1;
+				break;
+			case '?':
+				p1putstr(2, ("Usage: ./uspsv2 [-q <quantum in msec>] [workload file]\n"));
+			default: 
+				continue; 
+		}
 
 	//Check for usage errors that could occur
 
-    if(argc == 2){
-		fname = argv[1];
-	} else if(argc == 3){
-		quantum = atoi(argv[1]);
-		fname = argv[2];
-	} else{
-		p1putstr(2, ("Usage: ./uspsv1 [q <quantum in msec>] [workload file]\n"));
-		fd  = 0; 
-	} 
-												//Proceed to read from the specified file and execute commands from file
-	int j;
-	int wrd = 0; 
-	fd = open(fname, 0); 						// open file that was put in by the user
-	if(fd == -1){
-		perror("open");
-		return 1; 
-	}
-	while((j = p1getline(fd, buf, MAX)) != 0){ // while there is still a next line
-		if(j == -1){
-			p1putstr(2, "Error reading file line\n");
-			return EXIT_FAILURE;
+	if(qflag){
+		if(argc == 3){
+			quantum = argv[2];
+		} 
+		if(argc ==4 ){
+			quantum = argv[2];
+			fname =  argv[4];
 		}
 
-		wrd = p1getword(buf, wrd, programs);
-		/*while((p1getword(buf, wrd, arguments)) != -1){
-			printf("%d\n", wrd);
-		}*/
-		printf("here\n");
-	}
+	} else if(argc == 2){
+		//quantum = p1atoi(argv[1]);
 
-	close(fd);
+		char *p;
+		int val = -1; 
+		if((p = getenv("USPS_QUANTUM_MSEC")) != NULL){
+			val = p1atoi(p);
+		}
+		quantum = val; 
+
+		printf("quantum: %d\n", val);
+		fname = argv[1];
+
+													// Proceed to read from the specified file and execute commands from file
+		fd = open(fname, 0); 						// open file that was put in by the user
+		if(fd == -1){
+			p1putstr(2, "OPEN ERROR\n");
+			p1perror(fd, "open"); 						// error  
+			return EXIT_FAILURE; 
+		}
+		int progcount = 0;
+		int argcount = 0;
+		int j; 
+		int largest_line = 0; 
+		while((j = p1getline(fd, buf, BUFSIZ)) != 0){  // while there is still a next line
+			int index = 0; 
+			while((index = p1getword(buf, index, word)) != -1){ 
+				argcount++;
+			}
+			if(argcount > largest_line){
+				largest_line = argcount;
+			}
+			progcount++;
+			argcount = 0; 
+
+		}
+
+		//printf("prog count: %d\nlargest line: %d\n", progcount, largest_line);
+
+		// malloc a a 2d array that will become a 3d array with p1stdup()
+		programs = (char***)malloc(sizeof(char **) * (progcount+1));
+		for(int i = 0; i < progcount; i++){
+			programs[i] = (char**)malloc(sizeof(char*) * (largest_line+1));
+		}
+
+		close(fd);
+		fd = open(fname, 0);
+		parse(fd, programs);
+		close(fd);
+
+		executePrograms(programs, progcount);
+
+		printf("Done\n");
+
+	} else{
+
+		char buf[MAX];
+		char word[MAX];
+		char *prog[MAX]; 
+
+		char *p;
+		int val = -1; 
+		if((p = getenv("USPS_QUANTUM_MSEC")) != NULL){
+			val = p1atoi(p);
+		}
+		quantum = val; 
+
+
+		int index = 0; 
+		UNUSED int bytes; 
+		p1putstr(1, "Enter bash command: \n");
+		int i = 0; 
+
+		bytes = p1getline(0, buf, MAX);
+
+		while((i = p1getword(buf, i, word)) != -1){
+			prog[index] = p1strdup(word); 
+			index++;
+		}
+
+
+		int pid; 
+		//p1putstr(1, "here\n");
+		pid = fork();
+			switch(pid){
+
+			case -1:
+				fprintf(stderr, "Parent: fork failed\n");
+				exit(EXIT_FAILURE);
+			case 0: 
+				execvp(*prog, prog);
+				fprintf(stderr, "Child: execvp() failed\n");
+				exit(EXIT_FAILURE);
+			default:
+				wait(NULL); 
+			}
+		return EXIT_SUCCESS;
+		printf("rip\n");
+	}	
 	return EXIT_SUCCESS;
 }
